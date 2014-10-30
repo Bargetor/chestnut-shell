a0a9822a8451ced182f295d415c3d08fe495e5b3
Bargetor <bargetor@gmail.com>
2014-10-30 15:34:20 +0800

    init

---
+++ chestnut-shell.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: chestnut shell
+Plugin URI: http://chestnut.bargetor.com
+Description: chestnut 微信平台 wordpress 插件
+Version: 1.0.0
+Author: bargetor
+Author URI: http://www.bargetor.com
+*/
+?>
